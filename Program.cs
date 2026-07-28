6a5c09be14ab37dd97293b0dddee461adfae3341
Ruper-t <arrietanoriegaeduardo@gmail.com>
2026-07-28 16:43:54 -0600

    Formatear y Procesar Distancia

--- Program.cs
+++ Program.cs
@@ -4,6 +4,26 @@ public class FormatoDistancia
 {
 	public static void Main()
 	{
-		
+		//Procesar distancia, y darle formato 
+		double numeroPorProcesar = 1.0;
+		if (numeroPorProcesar < 0)
+		{
+			Console.WriteLine("Su distancia seria " + (numeroPorProcesar * 100) + "Centimetros");
+		}
+		else if (numeroPorProcesar == 0)
+		{
+			Console.WriteLine("No hay distancia para procesar");
+		}
+
+		else if(numeroPorProcesar < 1000 )
+		{
+			Console.WriteLine("Su distancia seria " + (numeroPorProcesar / 1000) + "Kilometros");
+		}			
+		else 
+		{
+			Console.WriteLine("Su distancia seria " + numeroPorProcesar + "Metros");
+		}
 	}
+
 }
+
