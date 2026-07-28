25caf97b7a2bfb297a8f1bacf1f33e582f9b829d
HelenGonzalez <hgonzalezkglander@gmail.com>
2026-07-28 16:57:27 -0600

    Borrar espacios

--- Program.cs
+++ Program.cs
@@ -5,11 +5,11 @@ public class FormatoDistancia
 	public static void Main()
 	{
 	
-	//Introducir distancia
-		Console.WriteLine ("Escribe la distancia :");
-		string distancia = Console.ReadLine ();
+		//Introducir distancia
+		Console.WriteLine("Escribe la distancia :");
+		string distancia = Console.ReadLine();
 		
-		double convertirDistancia =  double.Parse(distancia);
+		double convertirDistancia = double.Parse(distancia);
 		
 	}
 }
