277091e10060ea2b165b55cd4cc83fe506d1f183
Ruper-t <arrietanoriegaeduardo@gmail.com>
2026-07-29 14:22:34 -0600

    correccion-procesar-distancia

--- Program.cs
+++ Program.cs
@@ -7,18 +7,18 @@ public class FormatoDistancia
 		//Procesar distancia, y darle formato 
 		double numeroPorProcesar = 0.0;
 		string numeroProcesado;
-		
+
 		if (numeroPorProcesar < 0)
 		{
 			numeroProcesado = numeroPorProcesar * 100 + "cm";
 		}
 		else if(numeroPorProcesar > 1000 )
 		{
-			numeroProcesado = numeroPorProcesar / 1000 + "Km";
+			numeroProcesado = numeroPorProcesar / 1000 + "km";
 		}			
 		else 
 		{
-			numeroProcesado = numeroPorProcesar + "M";
+			numeroProcesado = numeroPorProcesar + "m";
 		}
 	}
 }
